5575687d65b3c8b0db843f9858879abb7497f09a
jdeastin <j.d.east@icloud.com>
2024-04-17 21:51:16 -0700

    worked itemlist to begin adding dynamic functionality to it later on

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { initalItems } from "../lib/constants";
+
 export default function ItemList() {
+  const [item, setItems] = useState(initalItems);
+
   return (
-    <div>ItemList</div>
-  )
+    <ul>
+      {item.map((item) => {
+        return <Item key={item.id} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input checked={item.packed} type="checkbox" />
+        {item.name}
+      </label>
+
+      <button>❌</button>
+    </li>
+  );
 }
